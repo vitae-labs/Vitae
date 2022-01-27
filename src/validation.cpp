604ef95850ca8d34564def5725e692df93ca424a
kalilinuxoo7 <hemant.singh.leu@gmail.com>
2022-01-27 10:08:10 +0530

    validation

--- src/validation.cpp
+++ src/validation.cpp
@@ -1994,20 +1994,20 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
     }
 
     // moved from CheckBlock() - funky behaviour there
-//    uint256 hashProofOfStake = uint256();
-//    if (block.IsProofOfStake() && pindex->nHeight > FORK_HEIGHT)
-//    {
-//        uint256 hash = block.GetHash();
-//        if(!CheckProofOfStake(block, hashProofOfStake, pindexPrev))
-//           return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "bad-stake", "check proof-of-stake failed");
+    uint256 hashProofOfStake = uint256();
+    if (block.IsProofOfStake() && pindex->nHeight > FORK_HEIGHT)
+    {
+        uint256 hash = block.GetHash();
+        if(!CheckProofOfStake(block, hashProofOfStake, pindex->pprev))
+           return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "bad-stake", "check proof-of-stake failed");
 
-//        if(hashProofOfStake == uint256())
-//           return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "bad-stake", "hashproof is null");
+        if(hashProofOfStake == uint256())
+           return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "bad-stake", "hashproof is null");
 
-//        if(!mapProofOfStake.count(hash)) { // add to mapProofOfStake
-//            mapProofOfStake.emplace(std::make_pair(hash, hashProofOfStake));
-//        }
-//    }
+        if(!mapProofOfStake.count(hash)) { // add to mapProofOfStake
+            mapProofOfStake.emplace(std::make_pair(hash, hashProofOfStake));
+        }
+    }
 
     // verify that the view's current state corresponds to the previous block
     uint256 hashPrevBlock = pindex->pprev == nullptr ? uint256() : pindex->pprev->GetBlockHash();
@@ -3879,8 +3879,7 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, Block
 
     if (!accepted_header)
         return false;
-    if(pindex->pprev && !pindex->pprev->IsValid(BLOCK_VALID_TRANSACTIONS))
-        return false;
+
 
     // Try to process all requested blocks that we don't have, but only
     // process an unrequested block if it's new and has enough work to
@@ -3913,6 +3912,10 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, Block
         // low-work blocks on a fake chain that we would never
         // request; don't process these.
         if (pindex->nChainWork < nMinimumChainWork) return true;
+
+        if(pindex->pprev && !pindex->pprev->IsValid(BLOCK_VALID_TRANSACTIONS))
+            return true;
+
     }
 
     if (!CheckBlock(block, state, chainparams.GetConsensus()) ||
